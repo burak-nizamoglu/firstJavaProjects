1129c05970566e11e282f05137d132fada61db9a
Burak Nizamoğlu <buraknizamoglu@mail.com>
2022-09-05 17:23:05 +0300

    newproject

---
+++ java/forPractice/src/packageForPractice/dikUcgendeHipotenusuBulanProgram.java
@@ -0,0 +1,23 @@
+package packageForPractice;
+
+import java.util.Scanner;
+
+public class dikUcgendeHipotenusuBulanProgram {
+    public static void main(String[] args) {
+        
+        System.out.println("Bu program iki dik kenarı kullanıcı tarafından girilen bir dik üçgenin hipotenüsünü hesaplar.");
+        Scanner inp = new Scanner(System.in);
+        int birinciDikKenar, ikinciDikKenar;
+        System.out.println("Birinci dik kenarı giriniz.");
+        System.out.print("Birinci dik kenar : ");
+        birinciDikKenar = inp.nextInt();
+        System.out.println("İkinci dik kenarı giriniz.");
+        System.out.print("İkinci dik kenar : ");
+        ikinciDikKenar = inp.nextInt();
+        inp.close();
+        
+        double hipotenus = Math.sqrt((birinciDikKenar*birinciDikKenar)+ (ikinciDikKenar*ikinciDikKenar));
+        System.out.println("Hipotenüs : " + hipotenus);
+    }
+    
+}
