465b4f4d01c28127e4816e87a9b4b55f4b29fb2f
Burak Nizamoğlu <buraknizamoglu@mail.com>
2022-09-05 17:37:21 +0300

    changed

--- java/forPractice/src/packageForPractice/kdvTutariHesaplayanProgram.java
+++ java/forPractice/src/packageForPractice/kdvTutariHesaplayanProgram.java
@@ -8,16 +8,17 @@ public class kdvTutariHesaplayanProgram {
         System.out.print("Aldığınız ürünün fiyatını giriniz. : ");
         
         double fiyat = inp.nextDouble();
-        double kdvtutari,kdvlifiyat,kdvorani;
-        System.out.print("KDV oranını giriniz : ");
-        kdvorani = inp.nextDouble();
-
+        double kdvtutari,kdvlifiyat;
+        
         inp.close();
-
-        kdvtutari = fiyat / kdvorani;
+        double kdvOrani = fiyat >1000 ? 0.08 : 0.18;
+        System.out.println("KDV Oranı : " + kdvOrani);
+        kdvtutari = fiyat * kdvOrani;
+        
         System.out.println("KDV Tutarı bu kadardır : " + kdvtutari);
-
+        
         kdvlifiyat = fiyat + kdvtutari;
+        
         System.out.println("KDV'li fiyat bu kadardır : " + kdvlifiyat);
     }   
 }
