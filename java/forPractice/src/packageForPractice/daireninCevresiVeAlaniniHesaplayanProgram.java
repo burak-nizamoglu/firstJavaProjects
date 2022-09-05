6725a4936658e90adfb8662a10fc181a38e21a4c
Burak Nizamoğlu <buraknizamoglu@mail.com>
2022-09-05 18:46:52 +0300

    changed v2

--- java/forPractice/src/packageForPractice/daireninCevresiVeAlaniniHesaplayanProgram.java
+++ java/forPractice/src/packageForPractice/daireninCevresiVeAlaniniHesaplayanProgram.java
@@ -7,10 +7,14 @@ public class daireninCevresiVeAlaniniHesaplayanProgram {
         Scanner inp = new Scanner(System.in);
         System.out.print("Dairenin yarıçapını giriniz :");
         int r = inp.nextInt();
-        inp.close();
         double pi=3.14,daireninAlani = r*r*pi ,daireninCevresi = 2*pi*r;
         System.out.println("Dairenin Alanı: " + daireninAlani);
         System.out.println("Dairenin Çevresi: " + daireninCevresi);
+        System.out.println("Belirli bir kesitin alanını bulmak için bir açı değeri giriniz (BU DEĞER ALFA OLSUN) :");
+        double alfa = inp.nextDouble();
+        double kesitAlani = daireninAlani*alfa/360;
+        System.out.println("Kesit alanı : " + kesitAlani);
+        inp.close();
 
     }
 }
