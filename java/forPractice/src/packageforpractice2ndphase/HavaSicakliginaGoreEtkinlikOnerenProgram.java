a89ae7f4bd4f6638a70e9a6f54e6fc223e39f76a
Burak Nizamoğlu <buraknizamoglu@mail.com>
2022-09-14 16:17:10 +0300

    HavaSicakliginaGoreEtkinlikOnerenProgram

---
+++ java/forPractice/src/packageforpractice2ndphase/HavaSicakliginaGoreEtkinlikOnerenProgram.java
@@ -0,0 +1,42 @@
+package packageforpractice2ndphase;
+
+import java.util.Scanner;
+
+public class HavaSicakliginaGoreEtkinlikOnerenProgram {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        System.out.print("Weather Degree: ");
+        double temperature = inp.nextDouble();
+        if(temperature<5) {
+            System.out.println("Skiing is a good option at this temperature.");
+        }
+        else if (temperature<=25 && temperature>=5) {
+            if(temperature>=5 && temperature<=15) {
+                if (temperature<=10) {
+                    System.out.println("Cinema is a good option at this temperature. ");
+                }
+                else {
+                    System.out.println("Cinema and picnic are good option at this temperature.");
+                }
+            }
+            else {
+                System.out.println("Picnic is a good option at this temperature.");
+            }
+        }
+        else if(temperature>25) {
+            System.out.println("Swimming is a good option at this temperature.");
+        }
+        inp.close();
+    }
+    
+}
+
+
+/*
+Koşullar :
+Sıcaklık 5'dan küçük ise "Kayak" yapmayı öner.
+Sıcaklık 5 ve 15 arasında ise "Sinema" etkinliğini öner.
+Sıcaklık 15 ve 25 arasında ise "Piknik" etkinliğini öner.
+Sıcaklık 25'ten büyük ise "Yüzme" etkinliğini öner.
+
+Aynı örnek üzerinden if koşulları başka hangi şekilde oluşturulabilirdi farklı çözüm yolları bulunuz. */
