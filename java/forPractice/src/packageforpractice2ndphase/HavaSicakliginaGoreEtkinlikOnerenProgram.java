ffa85d23c8fa596e4c98efbf737d2aae34dc588d
Burak Nizamoğlu <buraknizamoglu@mail.com>
2022-09-14 16:19:04 +0300

    HavaSicakliginaGoreEtkinlikOnerenProgram

--- java/forPractice/src/packageforpractice2ndphase/HavaSicakliginaGoreEtkinlikOnerenProgram.java
+++ java/forPractice/src/packageforpractice2ndphase/HavaSicakliginaGoreEtkinlikOnerenProgram.java
@@ -28,7 +28,6 @@ public class HavaSicakliginaGoreEtkinlikOnerenProgram {
         }
         inp.close();
     }
-    
 }
 
 
@@ -36,7 +35,6 @@ public class HavaSicakliginaGoreEtkinlikOnerenProgram {
 Koşullar :
 Sıcaklık 5'dan küçük ise "Kayak" yapmayı öner.
 Sıcaklık 5 ve 15 arasında ise "Sinema" etkinliğini öner.
-Sıcaklık 15 ve 25 arasında ise "Piknik" etkinliğini öner.
-Sıcaklık 25'ten büyük ise "Yüzme" etkinliğini öner.
+Sıcaklık 10 ve 25 arasında ise "Piknik" etkinliğini öner.
+Sıcaklık 25'ten büyük ise "Yüzme" etkinliğini öner. */
 
-Aynı örnek üzerinden if koşulları başka hangi şekilde oluşturulabilirdi farklı çözüm yolları bulunuz. */
