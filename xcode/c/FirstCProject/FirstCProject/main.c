d8f5b73cec6f7df650f6633195b0bde6e9c169f2
Burak Nizamoğlu <buraknizamoglu@mail.com>
2022-09-20 15:39:38 +0300

    changed

---
+++ xcode/c/FirstCProject/FirstCProject/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  FirstCProject
+//
+//  Created by Burak Nizamoğlu on 17.09.2022.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
