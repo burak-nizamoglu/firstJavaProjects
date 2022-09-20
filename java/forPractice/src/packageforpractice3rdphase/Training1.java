6d8fbecf683af7cd2953dfeea965ae177d821c97
Burak Nizamoğlu <buraknizamoglu@mail.com>
2022-09-20 15:41:10 +0300

    Training1

--- java/forPractice/src/packageforpractice3rdphase/Training1.java
+++ java/forPractice/src/packageforpractice3rdphase/Training1.java
@@ -4,7 +4,7 @@ public class Training1 {
     public static void main(String[] args) {
         int i=10;
         while (i<=10 && i>=0) {
-            System.out.println(--i);
+            System.out.println(i--);
         }
     }
 }
